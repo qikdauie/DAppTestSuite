5e0fda19712e8c2ccccc0a426ac75615018dbcd0
qikdauie <wyatt@qikfox.com>
2025-09-25 13:40:59 -0500

    update submodules storage and fix vite config for domain

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,22 @@ export default defineConfig({
   resolve: {
     preserveSymlinks: true,
     alias: [
-      { find: /^decent_app_sdk\/service-worker$/, replacement: pathResolve(__dirname, 'decent_app_sdk/src/service-worker/index.js') },
-      { find: /^decent_app_sdk\/client$/, replacement: pathResolve(__dirname, 'decent_app_sdk/src/client/index.js') },
-      { find: /^decent_app_sdk\/singleton$/, replacement: pathResolve(__dirname, 'decent_app_sdk/src/client/singleton.js') },
-      { find: /^decent_app_sdk\/protocols$/, replacement: pathResolve(__dirname, 'decent_app_sdk/src/protocols/index.js') },
-      { find: /^decent_app_sdk\/components$/, replacement: pathResolve(__dirname, 'decent_app_sdk/src/components/index.js') },
-      { find: /^decent_app_sdk$/, replacement: pathResolve(__dirname, 'decent_app_sdk/src/client/index.js') },
+      { find: /^decent_app_sdk\/service-worker$/, replacement: pathResolve(__dirname, 'submodules/decent_app_sdk/src/service-worker/index.js') },
+      { find: /^decent_app_sdk\/client$/, replacement: pathResolve(__dirname, 'submodules/decent_app_sdk/src/client/index.js') },
+      { find: /^decent_app_sdk\/singleton$/, replacement: pathResolve(__dirname, 'submodules/decent_app_sdk/src/client/singleton.js') },
+      { find: /^decent_app_sdk\/protocols$/, replacement: pathResolve(__dirname, 'submodules/decent_app_sdk/src/protocols/index.js') },
+      { find: /^decent_app_sdk\/components$/, replacement: pathResolve(__dirname, 'submodules/decent_app_sdk/src/components/index.js') },
+      { find: /^decent_app_sdk$/, replacement: pathResolve(__dirname, 'submodules/decent_app_sdk/src/client/index.js') },
     ]
   },
+  
   server: {
     open: false,
     host: true,
     port: 3000,
   },
   preview: {
+    allowedHosts: ['localhost', '127.0.0.1', 'hammerhead-dapp-pyfch.ondigitalocean.app'],
     open: false,
     host: true,
     port: 8080,
